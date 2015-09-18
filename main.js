2eb1d11ccd01407e521d29e11ff2435685ebbf7e
Joanie <joaniep@yahoo.com>
2015-09-18 11:31:11 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+var students = [{
+ name: 'Liz',
+ age: 25,
+ city: 'Boulder'
+},{
+ name: 'Meghan',
+ age: 27,
+ city: 'Denver'
+},{
+ name: 'Trent',
+ age: 32,
+ city: 'Boulder'
+},{
+ name: 'Chelsea',
+ age: 24,
+ city: 'Boulder'
+},{
+ name: 'Amir',
+ age: 18,
+ city: 'Denver'
+}];
+
+for(var i = 0; i < students.length; i++) {
+	console.log(students[i].name);
+}
+//==============================
+console.log('')
+//=============== #1 ===========
+
+for(var i = 0; i < students.length; i++) {
+	console.log(students[i].age);
+}
+
+//==============================
+console.log('')
+//=============== #2 ===========
+
+for(var i = 0; i < students.length; i++) {
+	console.log(students[i].name, ',', students[i].city);
+}
+
+//==============================
+console.log('')
+//=============== #3 ===========
+
+for(var i = 0; i < students.length; i+=2) {
+	console.log(students[i].name, 'is from ', students[i].city);
+}
+
+//==============================
+console.log('')
+//=============== #4 ===========
+
+for(var i = 0; i < students.length; i++) {
+	if(students[i].age > 25){
+		console.log(students[i].name, 'is older than 25.');
+	}
+}
